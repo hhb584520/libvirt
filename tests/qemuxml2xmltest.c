45026d6ccaeca3371c55273ea1146561e738dabb
Lin Yang <lin.a.yang@intel.com>
2022-05-18 00:59:32 -0700

    conf: Introduce SGX EPC element into device memory xml
    With NUMA config:
    
    <devices>
      ...
      <memory model='sgx-epc'>
        <source>
          <nodemask>0-1</nodemask>
        </source>
        <target>
          <size unit='KiB'>512</size>
          <node>0</node>
        </target>
      </memory>
      ...
    </devices>
    
    Without NUMA config:
    
    <devices>
      ...
      <memory model='sgx-epc'>
        <target>
          <size unit='KiB'>512</size>
        </target>
      </memory>
      ...
    </devices>
    
    Signed-off-by: Lin Yang <lin.a.yang@intel.com>
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

--- tests/qemuxml2xmltest.c
+++ tests/qemuxml2xmltest.c
@@ -1456,6 +1456,9 @@ mymain(void)
     DO_TEST_CAPS_LATEST("channel-qemu-vdagent");
     DO_TEST_CAPS_LATEST("channel-qemu-vdagent-features");
 
+    DO_TEST_CAPS_VER("sgx-epc", "6.2.0");
+    DO_TEST_CAPS_LATEST("sgx-epc-numa");
+
  cleanup:
     if (getenv("LIBVIRT_SKIP_CLEANUP") == NULL)
         virFileDeleteTree(fakerootdir);
