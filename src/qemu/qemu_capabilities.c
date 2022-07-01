69e66e0e05875a79e421d662ddd5412b59f95f29
Haibin Huang <haibin.huang@intel.com>
2022-07-01 12:14:44 -0700

    conf: expose SGX feature in domain capabilities
    Extend hypervisor capabilities to include sgx feature. When available,
    the hypervisor supports launching an VM with SGX on Intel platfrom.
    The SGX feature tag privides additional details like section size and
    sgx1 or sgx2.
    
    Signed-off-by: Haibin Huang <haibin.huang@intel.com>
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
    Signed-off-by: Haibin Huang <haibin.huang@intel.com>

--- src/qemu/qemu_capabilities.c
+++ src/qemu/qemu_capabilities.c
@@ -6688,6 +6688,21 @@ virQEMUCapsFillDomainFeatureS390PVCaps(virQEMUCaps *qemuCaps,
     }
 }
 
+/**
+ * virQEMUCapsFillDomainFeatureSGXCaps:
+ * @qemuCaps: QEMU capabilities
+ * @domCaps: domain capabilities
+ *
+ * Take the information about SGX capabilities that has been obtained
+ * using the 'query-sgx-capabilities' QMP command and stored in @qemuCaps
+ * and convert it to a form suitable for @domCaps.
+ */
+static void
+virQEMUCapsFillDomainFeatureSGXCaps(virQEMUCaps *qemuCaps,
+                                    virDomainCaps *domCaps)
+{
+    virQEMUCapsSGXInfoCopy(&domCaps->sgx, qemuCaps->sgxCapabilities);
+}
 
 int
 virQEMUCapsFillDomainCaps(virQEMUCaps *qemuCaps,
@@ -6740,6 +6755,7 @@ virQEMUCapsFillDomainCaps(virQEMUCaps *qemuCaps,
     virQEMUCapsFillDomainFeatureGICCaps(qemuCaps, domCaps);
     virQEMUCapsFillDomainFeatureSEVCaps(qemuCaps, domCaps);
     virQEMUCapsFillDomainFeatureS390PVCaps(qemuCaps, domCaps);
+    virQEMUCapsFillDomainFeatureSGXCaps(qemuCaps, domCaps);
 
     return 0;
 }
